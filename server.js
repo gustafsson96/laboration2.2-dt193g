214856df7edf5cfba292e84a7a9592dcb968faa2
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-11-29 20:11:58 +0100

    Enable SSL in server.js for connecting to Render

--- server.js
+++ server.js
@@ -21,7 +21,11 @@ new pool = new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.PORT || 5432
+    port: process.env.PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
+
 });
 
 // Create and initiate a Hapi server
